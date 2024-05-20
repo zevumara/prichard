043cae406c3ba39a2e0fcec40a0454829d9d825d
zevumara <sebastiancamara@gmail.com>
2024-05-19 23:24:10 -0300

    Added integration with ChatGPT

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import OpenAI from "openai";
+import { config as configDotenv } from "dotenv";
+
+configDotenv();
 
 const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const openai = new OpenAI({
+  apiKey: process.env.API_KEY,
+});
 
 io.on("connection", (socket) => {
   console.log("a client has connected");
@@ -27,8 +34,28 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/app/index.html");
 });
 
+app.get("/loading.svg", (req, res) => {
+  res.sendFile(process.cwd() + "/app/loading.svg");
+});
+
 app.get("/control", (req, res) => {
   res.sendFile(process.cwd() + "/app/control.html");
+});
+
+app.use(express.json());
+
+app.post("/suggestions", async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const chat_completion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: prompt }],
+      model: "gpt-3.5-turbo",
+    });
+    res.json({ suggestions: chat_completion.choices[0].message.content });
+  } catch (error) {
+    res.status(500).json({ suggestions: error });
+    console.log("Error:", error);
+  }
 });
 
 server.listen(port, () => {
